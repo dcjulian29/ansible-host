2cc05b8ea5a1898d55cc24e1cd6652f4495f1aa0
Julian Easterling <julian@julianscorner.com>
2023-09-13 10:50:51 -0400

    Added ability to get json, toml, and yaml from inventory and allow only one host to be shown

--- cmd/inventory.go
+++ cmd/inventory.go
@@ -16,27 +16,48 @@ limitations under the License.
 package cmd
 
 import (
+	"strings"
+
 	"github.com/spf13/cobra"
 )
 
 var (
 	inventoryCmd = &cobra.Command{
-		Use:     "inventory",
+		Use:     "inventory [hostname]",
 		Aliases: []string{"inv"},
 		Short:   "Show inventory information for the Ansible environment",
 		Long:    "Show inventory information for the Ansible environment",
 		Run: func(cmd *cobra.Command, args []string) {
 			inventory, _ := cmd.Flags().GetString("inventory")
+			limit, _ := cmd.Flags().GetStringSlice("subset")
 
-			executeExternalProgram("ansible-inventory", "-i", inventory, "--list", "--yaml")
+			param := []string{
+				"-i", inventory,
+				"-l", strings.Join(limit, ","),
+			}
+
+			if r, _ := cmd.Flags().GetBool("toml"); r {
+				param = append(param, "--toml")
+			}
+
+			if r, _ := cmd.Flags().GetBool("yaml"); r {
+				param = append(param, "--yaml")
+			}
+
+			if len(args) > 0 {
+				param = append(param, "--host", args[0])
+			} else {
+				param = append(param, "--list")
+			}
+
+			executeExternalProgram("ansible-inventory", param...)
 		},
 		PreRun: func(cmd *cobra.Command, args []string) {
 			ensureAnsibleDirectory()
-			if len(command) > 0 {
-				inventory, _ := cmd.Flags().GetString("inventory")
 
-				ensurefileExists(inventory, "Ansible inventory file is not accessable!")
-			}
+			inventory, _ := cmd.Flags().GetString("inventory")
+
+			ensurefileExists(inventory, "Ansible inventory file is not accessable!")
 		},
 		PostRun: func(cmd *cobra.Command, args []string) {
 			ensureWorkingDirectory()
@@ -48,5 +69,9 @@ func init() {
 	rootCmd.AddCommand(inventoryCmd)
 
 	inventoryCmd.Flags().StringP("inventory", "i", "hosts.ini", "inventory file for use with Ansible")
+	inventoryCmd.Flags().StringSliceP("subset", "l", []string{"all"}, "limit to specified subset")
+	inventoryCmd.Flags().Bool("toml", false, "Use TOML format instead of default JSON")
+	inventoryCmd.Flags().BoolP("yaml", "y", false, "Use TOML format instead of default JSON")
 
+	inventoryCmd.MarkFlagsMutuallyExclusive("toml", "yaml")
 }
